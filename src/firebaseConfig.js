bd6cdc4acfda15d4a7b03dd0c3363cebd8b4de6c
Lindahehexd <tkd09152129@gmail.com>
2023-01-17 19:58:16 +0800

    test

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,20 +3,17 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.CHAT_APP_API_KEY,
-  authDomain: process.env.CHAT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.CHAT_DATABASE_URL,
-  projectId:process.env.CHAT_APP_PROJECT_ID,
-  storageBucket:process.env.CHAT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.CHAT_APP_MESSAGING_SENDER_ID,
-  appId:process.env.CHAT_APP_APP_ID
+  apiKey: "AIzaSyBaLiC99o6g7Tof6ABj7qRxGE6bY16sCkE",
+  authDomain: "chat-app-b82b4.firebaseapp.com",
+  projectId: "chat-app-b82b4",
+  storageBucket: "chat-app-b82b4.appspot.com",
+  messagingSenderId: "308528732710",
+  appId: "1:308528732710:web:b22068fdc3b721d63b24e7",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
-
 export { auth, provider, db };
